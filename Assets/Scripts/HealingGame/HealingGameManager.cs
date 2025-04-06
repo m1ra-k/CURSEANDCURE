83560b444d646e6a6fbaf48fd422f9db2e94b7f0
Mira <mirakhan.mk910@gmail.com>
2025-04-06 10:35:28 -0400

    added slider that goes up/down with z/enter hold

--- Assets/Scripts/HealingGame/HealingGameManager.cs
+++ Assets/Scripts/HealingGame/HealingGameManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealingGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // just lilith for now
+    public string mode = "Lilith";
+    
+    [SerializeField]    
+    private Slider healingGauge;
+    private float healingSpeed = 0.5f;
+    private float healingGaugeValue = 0f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        AdjustHealingGauge();
+    }
+
+    private void AdjustHealingGauge()
+    {
+        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Return))
+        {
+            healingGaugeValue += healingSpeed * Time.deltaTime;
+        }
+        else
+        {
+            healingGaugeValue -= healingSpeed/2 * Time.deltaTime;
+        }
+
+        healingGaugeValue = Mathf.Clamp01(healingGaugeValue);
+
+        healingGauge.value = healingGaugeValue;
     }
 }
