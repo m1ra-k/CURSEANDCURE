295b201fb4ca675d5c1a9c677ea60e5020f5fe7e
Mira <mirakhan.mk910@gmail.com>
2025-04-06 19:55:14 -0400

    only triggered when fully inside the range

--- Assets/Scripts/HealingGame/HandleBehavior.cs
+++ Assets/Scripts/HealingGame/HandleBehavior.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class HandleBehavior : MonoBehaviour
 {
+    private Collider2D handleCollider;
+
+    void Start()
+    {
+        handleCollider = GetComponent<Collider2D>();
+    }
+
     void OnTriggerStay2D(Collider2D other)
     {
-        Debug.Log("Handle collided with: " + other.name);
+        if (HandleFullyInside(handleCollider, other))
+        {
+            Debug.Log("fully inside trigger: " + other.name);
+        }
+    }
+
+    bool HandleFullyInside(Collider2D inner, Collider2D outer)
+    {
+        Bounds innerBounds = inner.bounds;
+        Bounds outerBounds = outer.bounds;
+
+        return outerBounds.Contains(innerBounds.min) && outerBounds.Contains(innerBounds.max);
     }
 }
