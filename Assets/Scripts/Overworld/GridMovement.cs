723ab83c828a692c9d5b7c291270d5b23fbc242d
Mira Khan <115938924+m1ra-k@users.noreply.github.com>
2025-04-12 15:37:09 -0400

    mira-auto-talk (#7)
    * two types of auto, repeated (push back) and one time, need to fix bug tho
    
    * fixed bug (for one time talking it didnt properly do the delay if statement)

--- Assets/Scripts/Overworld/GridMovement.cs
+++ Assets/Scripts/Overworld/GridMovement.cs
@@ -9,12 +9,14 @@ public class GridMovement : MonoBehaviour
 
     [Header("[Properties]")]
     public string directionFacing = "down";
+    public Vector2 movementVector;
+    public bool isMoving;
+    public bool overrideIsMoving;
 
-    private Vector2 movementVector;
+
     private float moveSpeed = 5f;
     private float stepSize = 1f;
     private Vector2 targetPosition;
-    private bool isMoving = false;
 
     void Start() 
     {
@@ -31,47 +33,76 @@ public class GridMovement : MonoBehaviour
 
     void Move() 
     {
-        if (!isMoving && !GameProgressionManagerInstance.currentlyTalking)
+        if (!overrideIsMoving)
         {
-            if (Input.GetKey(KeyCode.UpArrow)) 
+            if (GameProgressionManagerInstance.currentlyTalking)
             {
-                movementVector = Vector2.up;
-                directionFacing = "up";
-            }
-            else if (Input.GetKey(KeyCode.DownArrow)) 
-            {
-                movementVector = Vector2.down;
-                directionFacing = "down";
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow)) 
-            {
-                movementVector = Vector2.left;
-                directionFacing = "left";
-            }
-            else if (Input.GetKey(KeyCode.RightArrow)) 
-            {
-                movementVector = Vector2.right;
-                directionFacing = "right";
+                isMoving = false;
+                movementVector = Vector2.zero;
             }
             else
             {
-                movementVector = Vector2.zero;
-            }
+                if (!isMoving)
+                {
+                    if (Input.GetKey(KeyCode.UpArrow)) 
+                    {
+                        movementVector = Vector2.up;
+                        directionFacing = "up";
+                    }
+                    else if (Input.GetKey(KeyCode.DownArrow)) 
+                    {
+                        movementVector = Vector2.down;
+                        directionFacing = "down";
+                    }
+                    else if (Input.GetKey(KeyCode.LeftArrow)) 
+                    {
+                        movementVector = Vector2.left;
+                        directionFacing = "left";
+                    }
+                    else if (Input.GetKey(KeyCode.RightArrow)) 
+                    {
+                        movementVector = Vector2.right;
+                        directionFacing = "right";
+                    }
+                    else
+                    {
+                        movementVector = Vector2.zero;
+                    }
 
-            if (movementVector != Vector2.zero) 
+                    if (movementVector != Vector2.zero) 
+                    {
+                        targetPosition = (Vector2) transform.position + movementVector * stepSize;
+                        isMoving = true;
+                    }
+                }
+                else 
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+                    if ((Vector2) transform.position == targetPosition) 
+                    {
+                        isMoving = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (!isMoving)
             {
                 targetPosition = (Vector2) transform.position + movementVector * stepSize;
                 isMoving = true;
             }
-        }
-        if (isMoving) 
-        {
-            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-
-            if ((Vector2) transform.position == targetPosition) 
+            else
             {
-                isMoving = false;
-            }
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+                if ((Vector2) transform.position == targetPosition) 
+                {
+                    isMoving = false;
+                    overrideIsMoving = false;
+                }
+            }    
         }
     }
 }
