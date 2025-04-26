de344f9b757e699ee367685b9fd700fbc284b4d2
Mira <mirakhan.mk910@gmail.com>
2025-04-26 13:06:24 -0400

    runs and faces correct direction but i want it to be smoother, not moving by units

--- Assets/Scripts/Overworld/NPCRun.cs
+++ Assets/Scripts/Overworld/NPCRun.cs
@@ -1,33 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCRun : MonoBehaviour
 {
+    private Animator animator;
+
     private int frameNumber = 0;
 
+    private Vector2 movementDirection;
     [SerializeField]
     private Vector2[] pathCoordinates;
     private int index;
 
+    private Dictionary<Vector2, string> animationLookup = new Dictionary<Vector2, string>
+    {
+        { Vector2.up, "Male1NPCWalkUp" },
+        { Vector2.down, "Male1NPCWalkDown" },
+        { Vector2.left, "Male1NPCWalkLeft" },
+        { Vector2.right, "Male1NPCWalkRight" }
+    };
+    private string currentAnimation = "Male1NPCWalkUp";
+    private string possibleAnimation;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
+        CheckSwitchAnimation();
+
         if (frameNumber == 10)
         {
-            Vector2 direction = Vector2.zero;
+            movementDirection = Vector2.zero;
 
             if (transform.localPosition.x != pathCoordinates[index].x)
             {
-                print("x was not the same");
-                direction.x = pathCoordinates[index].x > transform.localPosition.x ? 1 : -1;
+                movementDirection.x = pathCoordinates[index].x > transform.localPosition.x ? 1 : -1;
             }
             else if (transform.localPosition.y != pathCoordinates[index].y)
             {
-                print("y was not the same");
-                direction.y = pathCoordinates[index].y > transform.localPosition.y ? 1 : -1;
+                movementDirection.y = pathCoordinates[index].y > transform.localPosition.y ? 1 : -1;
             }
     
-            transform.localPosition = (Vector2) transform.localPosition + direction;
+            transform.localPosition = (Vector2) transform.localPosition + movementDirection;
 
             if ((Vector2) transform.localPosition == pathCoordinates[index])
             {
@@ -38,5 +57,19 @@ public class NPCRun : MonoBehaviour
         }
 
         frameNumber++;
+    }
+
+    private void CheckSwitchAnimation()
+    {
+        if (movementDirection != Vector2.zero)
+        {
+            possibleAnimation = animationLookup[movementDirection];
+
+            if (!currentAnimation.Equals(possibleAnimation))
+            {
+                animator.Play(possibleAnimation);
+                currentAnimation = possibleAnimation;
+            }
+        }
     }
 }
