34aafd619bb72f638415ea040c9e31bb3d2895e2
Mira <mirakhan.mk910@gmail.com>
2025-04-27 04:55:58 -0400

    game over uses z or enter instead of click

--- Assets/Scripts/GameOver/RetryButton.cs
+++ Assets/Scripts/GameOver/RetryButton.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RetryButton : MonoBehaviour
 {
     public GameProgressionManager GameProgressionManager;
 
+    private Button retryButton;
+
     void Awake()
     {
         GameProgressionManager = GameObject.Find("GameProgressionManager").GetComponent<GameProgressionManager>();
+    }
+
+    void Start()
+    {
+        retryButton = GetComponent<Button>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Return))
+        {
+            retryButton.onClick.Invoke();
+        }
     }
 
     public void RetryGame()
